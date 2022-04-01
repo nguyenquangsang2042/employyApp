8649a9efa754dac8e710f65c5c7d9d2f5866ef8e
binnn <34269455+nguyenquangsang2042@users.noreply.github.com>
2022-04-01 11:38:20 +0700

    custom headernavigation

--- App.js
+++ App.js
@@ -6,15 +6,24 @@ import Home from './screens/Home';
 import Profile from './screens/Profile';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { Title } from 'react-native-paper';
 
 const Stack= createNativeStackNavigator();
+const mycustomOptionsHeader={
+  title:"home",
+  headerTintColor:"white",
+  headerStyle:{
+    backgroundColor:"#006aff"
+  }
+}
 export default function App() {
+  
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name='Home' component={Home}/>
-        <Stack.Screen name='CreateUser' component={CreateEmployy}/>
-        <Stack.Screen name='profile' component={Profile}/>
+    <NavigationContainer >
+      <Stack.Navigator initialRouteName='Home' >
+        <Stack.Screen name='Home' component={Home} options={mycustomOptionsHeader}/>
+        <Stack.Screen name='CreateUser' options={{...mycustomOptionsHeader,title:"Create Employye"}} component={CreateEmployy}/>
+        <Stack.Screen name='profile' options={{...mycustomOptionsHeader,title:"Profile"}} component={Profile}/>
       </Stack.Navigator>
     </NavigationContainer>
     
