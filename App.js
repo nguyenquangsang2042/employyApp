a7e63cfaac336c1f4d1f98d91ca56aad9e750b02
sangnq2903 <34269455+nguyenquangsang2042@users.noreply.github.com>
2022-04-20 10:16:37 +0700

    drawer navigation

--- App.js
+++ App.js
@@ -4,9 +4,27 @@ import Home from './screens/Home';
 import Profile from './screens/Profile';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import 'react-native-gesture-handler';
+import { createDrawerNavigator, DrawerContentScrollView, DrawerItem, DrawerItemList } from '@react-navigation/drawer';
+import 'react-native-reanimated';
+const Drawer = createDrawerNavigator();
+function CustomDrawerContent(props) {
+  return (
+    <DrawerContentScrollView {...props}>
+      <DrawerItemList {...props} />
+    </DrawerContentScrollView>
+  );
+}
 
-
-
+function MyDrawer() {
+  return (
+    <Drawer.Navigator useLegacyImplementation={false} drawerContent={props=><CustomDrawerContent{...props}/>}>
+      <Drawer.Screen name="Feed" component={Home} />
+      <Drawer.Screen name="CreateUser" component={CreateEmployy} />
+      <Drawer.Screen name="profile" component={Profile} />
+    </Drawer.Navigator>
+  );
+}
 const Stack= createNativeStackNavigator();
 const mycustomOptionsHeader={
   title:"home",
@@ -18,11 +36,7 @@ const mycustomOptionsHeader={
 export default function App() {
   return (
     <NavigationContainer >
-      <Stack.Navigator initialRouteName='Home' >
-        <Stack.Screen name='Home' component={Home} options={mycustomOptionsHeader}/>
-        <Stack.Screen name='CreateUser' options={{...mycustomOptionsHeader,title:"Create Employye"}} component={CreateEmployy}/>
-        <Stack.Screen name='profile' options={{...mycustomOptionsHeader,title:"Profile"}} component={Profile}/>
-      </Stack.Navigator>
+    <MyDrawer/>      
     </NavigationContainer>
     
   );
